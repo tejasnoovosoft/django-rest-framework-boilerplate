9533ab90abeb5e891c5151584ecd96a6738c7074
Tejas Ekhande <tejas.ekhande@noovosoft.com>
2025-09-24 23:26:03 +0530

    Add likes_count in blog posts response

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,13 +1,18 @@
 from rest_framework import serializers
 
 from blog.models import Post
+from social.models import Like
 from user.serializers import UserSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
+    likes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = '__all__'
         read_only_fields = ['author', 'created_at']
+
+    def get_likes_count(self, post):
+        return Like.objects.filter(post=post).count()
