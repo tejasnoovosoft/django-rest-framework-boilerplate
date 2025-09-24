dd7acd180a0213269d853ad02f8b3a199825f09a
Tejas Ekhande <tejas.ekhande@noovosoft.com>
2025-09-24 22:50:18 +0530

    Add models for likes and comments

--- social/models.py
+++ social/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from blog.models import Post
+from user.models import User
 
-# Create your models here.
+
+class PostInteractionBase(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Like(PostInteractionBase):
+    class Meta:
+        unique_together = ('post', 'user')
+        verbose_name = "Like"
+        verbose_name_plural = "Likes"
+
+
+class Comment(PostInteractionBase):
+    comment = models.TextField()
+
+    class Meta:
+        ordering = ('-created_at',)
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
