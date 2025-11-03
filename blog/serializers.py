49079d1bd3d916cbde7358a0de9a9e496ba1ebf3
Tejas Ekhande <tejas.ekhande@noovosoft.com>
2025-11-03 15:32:54 +0530

    Optimize post fetching queries for better performance

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,14 +1,13 @@
 from rest_framework import serializers
 
 from blog.models import Post
-from social.models import Like, Comment
 from social.serializers import CommentSerializer
 from user.serializers import UserSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
-    likes_count = serializers.SerializerMethodField()
+    likes_count = serializers.IntegerField(read_only=True)
     comments = serializers.SerializerMethodField()
 
     class Meta:
@@ -16,9 +15,6 @@ class PostSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ['author', 'created_at']
 
-    def get_likes_count(self, post):
-        return Like.objects.filter(post=post).count()
-
     def get_comments(self, post):
-        comments = Comment.objects.filter(post=post).order_by('-created_at')
+        comments = post.comment_set.all()
         return CommentSerializer(comments, many=True).data
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
+from django.db.models import Count, Prefetch
 from rest_framework import viewsets, request
 from rest_framework.permissions import IsAuthenticated
 
 from blog.models import Post
 from blog.serializers import PostSerializer
+from social.models import Comment
 from user.enums import UserRole
 
 
@@ -12,10 +14,19 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
 
     def get_queryset(self):
+
+        queryset = Post.objects.select_related('author').prefetch_related(Prefetch(
+            'comment_set',
+            queryset=Comment.objects.select_related('user').order_by('-created_at')
+        )
+        ).annotate(
+            likes_count=Count('like', distinct=True)
+        )
+
         if self.request.user.role == UserRole.ADMIN.value:
-            return Post.objects.select_related('author').all()
+            return queryset.all()
         else:
-            return Post.objects.select_related('author').filter(author=self.request.user)
+            return queryset.filter(author=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
