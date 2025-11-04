2fb16d7a45946fb2aae97db8631b7de48d8e2466
Tejas Ekhande <tejas.ekhande@noovosoft.com>
2025-11-04 12:37:53 +0530

    Add category field in post model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from categories.models import Category
 from user.models import User
 
 
@@ -8,3 +9,4 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
